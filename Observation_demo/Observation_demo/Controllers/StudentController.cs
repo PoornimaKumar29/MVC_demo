bd7ddeff72b9b53df558f41995d86a75f89254bc
PoornimaKumar29 <poornimakumar2907@gmail.com>
2025-05-15 09:41:33 +0530

    crud operations

--- Observation_demo/Observation_demo/Controllers/StudentController.cs
+++ Observation_demo/Observation_demo/Controllers/StudentController.cs
@@ -1,68 +1,146 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using Observation_demo.Models;
+
+//namespace Observation_demo.Controllers
+//{
+//    public class StudentController : Controller
+//    {
+//        // dependecy injection demo
+//        private readonly IStudentRepo _studentRepo;
+
+
+//        //constructor dependecy injection 
+//        public StudentController(IStudentRepo studentRepo)
+//        {
+//            _studentRepo = studentRepo;
+
+//        }
+//        public JsonResult GetStudentdetails(int id)
+//        {
+//            //because of constructor dependecy no need to instance
+//            //StudentRepoImpl studentRepo = new StudentRepoImpl();
+//            Student Stude = _studentRepo.GetStudentById(id);
+//            return Json(Stude);
+//        }
+//        public IActionResult Index()
+//        {
+//            List<Student> Allstu = _studentRepo.GetAllStudents();
+//            return View(Allstu);
+//        }
+
+
+//        public JsonResult Create(int id, string name, string branch, string gender)
+//        {
+//            Student newStu = new Student { Id = id, Name = name, Branch = branch, Gender = gender };
+//            _studentRepo.AddStudent(newStu);
+//            return Json(new { message = "Student added" });
+//        }
+
+//        public JsonResult Edit(int id, string name, string branch, string gender)
+//        {
+//            Student updatedStu = new Student { Id = id, Name = name, Branch = branch, Gender = gender };
+//            _studentRepo.UpdateStudent(updatedStu);
+//            return Json(new { message = "Student updated" });
+//        }
+
+//        public JsonResult Delete(int id)
+//        {
+//            _studentRepo.DeleteStudent(id);
+//            return Json(new { message = "Student deleted" });
+//        }
+//        //public string GetAllStudents()
+//        //{
+//        //    return "All student data";
+//        //}
+//        //public string getstudentbyname(string name)
+//        //{
+//        //    return $"hii iam {name}";
+//        //}
+//        //public string getstudent()
+//        //{
+//        //    return $"hii ";
+//        //}
+
+
+
+
+//    }
+//}
+using Microsoft.AspNetCore.Mvc;
 using Observation_demo.Models;
 
 namespace Observation_demo.Controllers
 {
     public class StudentController : Controller
     {
-        // dependecy injection demo
         private readonly IStudentRepo _studentRepo;
-       
 
-        //constructor dependecy injection 
         public StudentController(IStudentRepo studentRepo)
         {
             _studentRepo = studentRepo;
-
         }
-        public JsonResult GetStudentdetails(int id)
+
+        public IActionResult Index()
         {
-            //because of constructor dependecy no need to instance
-            //StudentRepoImpl studentRepo = new StudentRepoImpl();
-            Student Stude = _studentRepo.GetStudentById(id);
-            return Json(Stude);
+            var students = _studentRepo.GetAllStudents();
+            return View(students);
         }
-        public JsonResult Index()
+
+        public IActionResult Create()
         {
-            List<Student> Allstu = _studentRepo.GetAllStudents();
-            return Json(Allstu);
+            return View();
         }
 
-
-        public JsonResult Create(int id, string name, string branch, string gender)
+        [HttpPost]
+        public IActionResult Create(Student student)
         {
-            Student newStu = new Student { Id = id, Name = name, Branch = branch, Gender = gender };
-            _studentRepo.AddStudent(newStu);
-            return Json(new { message = "Student added" });
+            if (ModelState.IsValid)
+            {
+                _studentRepo.AddStudent(student);
+                TempData["Message"] = "Student Added Successfully!";
+                return RedirectToAction("Index");
+            }
+            return View(student);
         }
 
-        public JsonResult Edit(int id, string name, string branch, string gender)
+        public IActionResult Edit(int id)
         {
-            Student updatedStu = new Student { Id = id, Name = name, Branch = branch, Gender = gender };
-            _studentRepo.UpdateStudent(updatedStu);
-            return Json(new { message = "Student updated" });
+            var student = _studentRepo.GetStudentById(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
         }
 
-        public JsonResult Delete(int id)
+        [HttpPost]
+        public IActionResult Edit(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _studentRepo.UpdateStudent(student);
+                TempData["Message"] = "Student Updated Successfully!";
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var student = _studentRepo.GetStudentById(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
         {
             _studentRepo.DeleteStudent(id);
-            return Json(new { message = "Student deleted" });
+            TempData["Message"] = "Student Deleted Successfully!";
+            return RedirectToAction("Index");
         }
-        //public string GetAllStudents()
-        //{
-        //    return "All student data";
-        //}
-        //public string getstudentbyname(string name)
-        //{
-        //    return $"hii iam {name}";
-        //}
-        //public string getstudent()
-        //{
-        //    return $"hii ";
-        //}
-
-
-
-
     }
 }
--- Observation_demo/Observation_demo/obj/Debug/net8.0/Observation_demo.AssemblyInfo.cs
+++ Observation_demo/Observation_demo/obj/Debug/net8.0/Observation_demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Observation_demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39fd7a3be4de69f398109f6c75e5207ea648b06d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Observation_demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Observation_demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
